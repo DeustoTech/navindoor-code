115dcfbac64dc9b07b7e761aae6b3147da5d8453
Jesus Oroya <djoroya@deusto.es>
2019-01-17 13:18:57 +0100

    fake algorithm

---
+++ WorkFolder/Tracking-Algorithms/fake/fake.m
@@ -0,0 +1,17 @@
+function mt_trajectory = fake(signals,ibuilding,itraj)
+    % description: fakes algoritm 
+       
+    tline = itraj.GroundTruths.Ref.timeline;
+    mt_trajectory = zeros(length(tline),4);
+    index = 0;
+    for t = tline
+        index = index + 1;
+        result = step(itraj,t);
+        mt_trajectory(index,1) = result.x + 2*(0.5-rand);
+        mt_trajectory(index,2) = result.y + 2*(0.5-rand);
+        mt_trajectory(index,3) = result.z;
+        mt_trajectory(index,4) = result.t;
+    end
+
+end
+
