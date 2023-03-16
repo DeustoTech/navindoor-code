076664f748f931d1681e5be3ba0327aa847eef69
Luis Enrique Díez <52312443+luis-enrique-diez@users.noreply.github.com>
2023-03-16 15:31:40 +0100

    Update step.m
    Typo: "previus" instead of "previous"

--- navindoor-source/src/CLI/CT02-signals/classes/@BeaconSgn/step.m
+++ navindoor-source/src/CLI/CT02-signals/classes/@BeaconSgn/step.m
@@ -2,7 +2,7 @@ function result = step(iBeaconSgn,t)
 %STEP Summary of this function goes here
 %   Detailed explanation goes here
     indexs = 1:length(iBeaconSgn.timeline);
-    index = interp1(iBeaconSgn.timeline,indexs,t,'previus','extrap');
+    index = interp1(iBeaconSgn.timeline,indexs,t,'previous','extrap');
     if isnan(index)
        error('The parameter t out range.') 
     end
