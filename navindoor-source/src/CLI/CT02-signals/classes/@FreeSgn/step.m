32faad240c0e0886c43352206b4b5354c00d48ab
Luis Enrique Díez <52312443+luis-enrique-diez@users.noreply.github.com>
2023-03-16 15:33:24 +0100

    Update step.m
    Type: "previus" instead of "previous"

--- navindoor-source/src/CLI/CT02-signals/classes/@FreeSgn/step.m
+++ navindoor-source/src/CLI/CT02-signals/classes/@FreeSgn/step.m
@@ -2,7 +2,7 @@ function result = step(iFreeSgn,t)
 %STEP Summary of this function goes here
 %   Detailed explanation goes here
     indexs = 1:length(iFreeSgn.timeline);
-    index = interp1(iFreeSgn.timeline,indexs,t,'previus','extrap');
+    index = interp1(iFreeSgn.timeline,indexs,t,'previous','extrap');
     if isnan(index)
        error('The parameter t out range.') 
     end
